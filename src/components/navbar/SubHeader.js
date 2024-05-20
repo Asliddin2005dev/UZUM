7902c12337907110a8b16902939ff20586c60e6c
Asliddin2005dev <asliddinqodirjonov820@gmail.com>
2024-05-20 16:58:53 +0500

    Yangilash

---
+++ src/components/navbar/SubHeader.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './navbar.css'
+
+function SubHeader() {
+  return (
+    <div className='subheader'>
+      <div className="container">
+        <div className="subheader-wrapper">
+          <div className="subheader-item">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="subheader-item">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="subheader-item">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SubHeader
